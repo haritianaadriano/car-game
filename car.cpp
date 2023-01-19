be2b02fbecff90a5a258675f8c0de97c6f71f716
haritiana adriano <hei.adriano.4@gmail.com>
2023-01-19 13:12:18 +0300

    feat: game over sorting in console

--- car.cpp
+++ car.cpp
@@ -54,9 +54,57 @@ void genEnemy(int ind){
 }
 void drawEnemy(int ind){
 	if(enemyFlag[ind] == true){
-		gotoxy(enemyX[ind], enemy[ind]); cout <<"****";
-		gotoxy(enemyX[ind], enemy[ind]+1); cout <<" ** ";
+		gotoxy(enemyX[ind], enemyY[ind]); cout <<"****";
+		gotoxy(enemyX[ind], enemyY[ind]+1); cout <<" ** ";
 		gotoxy(enemyX[ind], enemyY[ind]+2; cout<<"****";
 		gotoxy(enemyX[ind], enemyY[ind]+3); cout<<" ** ";
 	}
 }
+void eraseEnemy (int ind){
+	if(enemyFlag[ind] == true){
+		gotoxy(enemyX[ind], enemyY[ind]); cout <<"    ";
+		gotoxy(enemyX[ind], enemyY[ind]+1; cout <<"    ";
+		gotoxy(enemyX[ind], enemyY[ind]+2); cout <<"    ";
+		gotoxy(enemyX[ind], enemyY[ind]+3); cout <<"    ";
+	}
+}
+void resetEnemy(int ind){
+	eraseEnemy(ind);
+	enemyY[ind] = 1;
+	genEnemy(ind);
+}
+void drawCar(){
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			gotoxy(j+carPos, i+22); cout<<car[i][j];
+		}
+	}
+}
+void eraseCar(){
+	for(int i=0; i<4; i++){
+		for(int j=0; j<4; j++){
+			gotoxy(j+carPos, i+22); cout<<"  ";
+		}
+	}
+}
+int collision(){
+	if(enemyY[0]+4 >= 23){
+		if(enemyX[0] + 4 - carPos >= 0 && enemyX[0] + 4 - carPos < 9){
+			return 1;
+		}
+	}
+	return 0;
+}
+void gameover(){
+	system("cls");
+	cout<<endl;
+	cout<<"\t\t--------------------------------------"<<endl;
+	cout<<"\t\t----------------Game Over-------------"<<endl;
+	cout<<"\t\t--------------------------------------"<<endl<<endl;
+	cout<<"\t\tPress any key to go back to meny. ";
+	getch();
+}
+void updateScore(){
+	gotoxy(WIN_WIDTH + 7, 5);cout<<"Score: "<<score<<endl;
+} 
+ 
