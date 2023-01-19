af0ac0dbb14cc6db9db254e52bf7afbc23c079a8
haritiana adriano <hei.adriano.4@gmail.com>
2023-01-19 10:53:39 +0300

    feat: drawBorder function to draw the border of the game

--- car.cpp
+++ car.cpp
@@ -38,3 +38,15 @@ void setcursor(bool visible, DWORD size){
 	lpCursor.dwSize =size;
 	SetConsoleCursorInfo(console,&lpCursor);
 }
+void drawBorder(){
+	for(int i=0; i<SCREEN_HEIGHT; i++){
+		for(int j=0; j<17; j++){
+			gotoxy(0+j,i); cout<<"+";
+			gotoxy(WIN_WIDTH-j,i); cout<<"+";
+		}
+	}
+	for(int i=0; i<SCREEN_HEIGHT; i++){
+		gotoxy(SCREEN_WIDTH, i); cout<<"+";
+	}
+}
+void genEnemy
